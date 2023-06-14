5b94e87c0b3776a8da8c2ff4f5dbd2094be964c8
unknown <nsikansimon@gmail.com>
2023-06-14 01:15:37 +0100

    working on db

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
-const mysql = require('mysql');
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const db = new sqlite3.Database('DB/election.db');
+const db = new sqlite3.Database('DB/data.sqlite');
+
+// Perform a SELECT query
+db.all('SELECT * FROM agentname', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      // Process the query result
+      rows.forEach((row) => {
+        console.log(row);
+      });
+    }
+  
+    // Close the database connection
+    db.close();
+  });
+
+
 
 
 
